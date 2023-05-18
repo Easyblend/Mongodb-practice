62bc73a95987871ee10819b97336b94e03339542
Emmanuel Kumah <kenzieemma072@gmail.com>
2023-05-18 23:46:06 +0000

    working on front end for the simple finance racker

--- mern-stack-app/src/Pages/Deposit.jsx
+++ mern-stack-app/src/Pages/Deposit.jsx
@@ -1,5 +1,63 @@
 import React from "react";
 
 export const Deposit = () => {
-  return <div>Deposit</div>;
+  return (
+    <div className="container">
+      <h1
+        className="text-center mx-auto my-5 border-bottom border-4 border-dark"
+        style={{ width: "fit-content" }}>
+        Deposit Funds
+      </h1>
+      <div className="row mt-5">
+        <div className="col">
+          <div
+            className="bg-dark text-light rounded-3 p-4 "
+            style={{ width: "fit-content" }}>
+            <h6>Current Balance</h6>
+            <h1 className=" display-1 fw-bold">&#8373; 198.00</h1>
+          </div>
+
+          <div className="mt-5">
+            <form class="row g-3">
+              <div class="col-auto">
+                <label for="inputPassword2" class="visually-hidden">
+                  Password
+                </label>
+                <input
+                  type="number"
+                  class="form-control"
+                  id="inputPassword2"
+                  placeholder="Amount in &#8373;"
+                />
+              </div>
+              <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-3">
+                  Deposit &#8373;500
+                </button>
+              </div>
+            </form>
+          </div>
+          <div className="my-5">
+            <p className="text-secondary">
+              This is a Test for MongoDB noSQL database, You can laugh at <br />
+              Dont mind my UI, once again this is just a front end page to test
+              my backend
+            </p>
+            <div className="gap-2 d-flex mx-auto ">
+              <button className="btn btn-success">I laughed</button>
+              <button className="btn btn-secondary">I didn't</button>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-6">
+          <img
+            src="https://www.freepngimg.com/thumb/smartphone/68695-mobile-smartphone-design-iphone-apple-free-hd-image.png"
+            alt=""
+            width="100%"
+            height="100%"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
--- mern-stack-app/src/Pages/History.jsx
+++ mern-stack-app/src/Pages/History.jsx
@@ -1,5 +1,39 @@
 import React from "react";
 
 export const History = () => {
-  return <div>History</div>;
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5">All Transaction History</h1>
+
+      <table className="table mt-5">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Type</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Date</th>
+          </tr>
+        </thead>
+        <tbody className="table-group-divider">
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
